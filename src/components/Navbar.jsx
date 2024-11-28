68a873efaf345c483b3c7bf065549f7288474283
robitkun <muhammadrobitzainulhaq@gmail.com>
2024-11-28 15:40:11 +0700

    Mengubah style warna

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
 const Navbar = ({ value, onChangeKeyword }) => {
   return (
     <nav className="navbar">
-      <h1>KyuJayNotes</h1>
+      <h3>
+        <Link to="/">NotesApp</Link>
+      </h3>
       <input
         type="text"
-        placeholder="Cari catatan.."
+        placeholder="Cari catatan..."
         value={value}
         onChange={(e) => onChangeKeyword(e)}
       />
